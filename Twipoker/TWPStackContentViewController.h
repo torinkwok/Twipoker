6b3b5607ea799fe000608b7b2c39955fba2929fd
Tong Guo <Tong-G@outlook.com>
2015-04-29 16:31:33 +0800

    Introduced `TWPStackContentViewController` class then make its instance the datasource and delegate of instance of `TWPDashboardView`

--- Twipoker/TWPStackContentViewController.h
+++ Twipoker/TWPStackContentViewController.h
@@ -22,101 +22,22 @@
   ████████████████████████████████████████████████████████████████████████████████
   ██████████████████████████████████████████████████████████████████████████████*/
 
-#import "TWPDashboardViewController.h"
-#import "TWPDashboardView.h"
-#import "TWPDashboardCellView.h"
-#import "TWPDashboardStack.h"
+#import <Cocoa/Cocoa.h>
 
-@implementation TWPDashboardViewController
+@class TWPDashboardStack;
 
-@synthesize homeDashboardStack;
-@synthesize favoritesDashboardStack;
-@synthesize listsDashboardStack;
-@synthesize notificationsDashboardStack;
-@synthesize meDashboardStack;
-@synthesize messagesDashboardStack;
-
-#pragma mark Initialization
-- ( instancetype ) init
+@interface TWPStackContentViewController : NSViewController <NSTableViewDataSource, NSTableViewDelegate>
     {
-    if ( self = [ super init ] )
-        {
-        self->_dashboardTabs = @[ NSLocalizedString( @"Home", nil )
-                                , NSLocalizedString( @"Favorites", nil )
-                                , NSLocalizedString( @"Lists", nil )
-                                , NSLocalizedString( @"Notifications", nil )
-                                , NSLocalizedString( @"Me", nil )
-                                , NSLocalizedString( @"Messages", nil )
-                                ];
-        }
-
-    return self;
+@private
+    NSArray __strong* _dashboardTabs;
     }
 
-NSString static* const kColumnIDTabs = @"tabs";
-
-#pragma mark Conforms to <NSTableViewDataSource>
-- ( NSInteger ) numberOfRowsInTableView: ( NSTableView* )_TableView
-    {
-    return self->_dashboardTabs.count;
-    }
-
-- ( id )            tableView: ( NSTableView* )_TableView
-    objectValueForTableColumn: ( NSTableColumn* )_TableColumn
-                          row: ( NSInteger )_Row
-    {
-    id result = nil;
-
-    if ( [ _TableColumn.identifier isEqualToString: kColumnIDTabs ] )
-        result = self->_dashboardTabs[ _Row ];
-
-    return result;
-    }
-
-#pragma mark Conforms to <NSTableViewDelegate>
-- ( NSView* ) tableView: ( NSTableView* )_TableView
-     viewForTableColumn: ( NSTableColumn* )_TableColumn
-                    row: ( NSInteger )_Row
-    {
-    TWPDashboardCellView* dashboardCellView = [ _TableView makeViewWithIdentifier: _TableColumn.identifier owner: self ];
-
-    NSString* tabName = [ _TableView.dataSource tableView: _TableView objectValueForTableColumn: _TableColumn row: _Row ];
-    [ dashboardCellView.textField setStringValue: tabName ];
-
-    TWPDashboardStack* viewsStack = nil;
-    switch ( _Row )
-        {
-        case 0: viewsStack = self.homeDashboardStack; break;
-        case 1: viewsStack = self.favoritesDashboardStack; break;
-        case 2: viewsStack = self.listsDashboardStack; break;
-        case 3: viewsStack = self.notificationsDashboardStack; break;
-        case 4: viewsStack = self.meDashboardStack; break;
-        case 5: viewsStack = self.messagesDashboardStack; break;
-        }
-
-    dashboardCellView.associatedViewsStack = viewsStack;
-
-    // TODO: [ cellView.imageView set... ];
-
-    return dashboardCellView;
-    }
-
-- ( void ) tableViewSelectionDidChange: ( NSNotification* )_Notif
-    {
-    NSTableView* tabTableView = [ _Notif object ];
-    NSTableColumn* currentTableColumn = [ tabTableView tableColumnWithIdentifier: @"tabs" ];
-    NSInteger selectedRow = [ tabTableView selectedRow ];
-
-    TWPDashboardCellView* cellView = ( TWPDashboardCellView* )[ tabTableView.delegate
-        tableView: tabTableView viewForTableColumn: currentTableColumn row: ( NSInteger )selectedRow ];
-
-    NSNotification* notification = [ NSNotification notificationWithName: TWPDashboardViewSelectedTabChanged
-                                                                  object: self
-                                                                userInfo: @{ @"tab-cell-view" : cellView } ];
-
-    [ [ NSNotificationCenter defaultCenter ] postNotification: notification ];
-
-    }
+@property ( weak ) IBOutlet TWPDashboardStack* homeDashboardStack;
+@property ( weak ) IBOutlet TWPDashboardStack* favoritesDashboardStack;
+@property ( weak ) IBOutlet TWPDashboardStack* listsDashboardStack;
+@property ( weak ) IBOutlet TWPDashboardStack* notificationsDashboardStack;
+@property ( weak ) IBOutlet TWPDashboardStack* meDashboardStack;
+@property ( weak ) IBOutlet TWPDashboardStack* messagesDashboardStack;
 
 @end
 
