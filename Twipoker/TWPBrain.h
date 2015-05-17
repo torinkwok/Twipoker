970bc7d482bf0c4b7ca256df3fd69a19869b8082
Tong Guo <Tong-G@outlook.com>
2015-05-17 18:51:55 +0800

    Backup 🚀

--- Twipoker/TWPBrain.h
+++ Twipoker/TWPBrain.h
@@ -24,29 +24,53 @@
 
 #import <Foundation/Foundation.h>
 
+@protocol TWPLimb;
+
 typedef NS_ENUM ( NSUInteger, TWPBrainSignalTypeMask )
-    { TWPBrainSignalTypeTweetMask           = 1 << 0
-    , TWPBrainSignalTypeFriendsListsMask    = 1 << 1
-    , TWPBrainSignalTypeDeleteMask          = 1 << 2
-    , TWPBrainSignalTypeScrubGeoMask        = 1 << 3
-    , TWPBrainSignalTypeLimitMask           = 1 << 4
-    , TWPBrainSignalTypeDisconnectMask      = 1 << 5
-    , TWPBrainSignalTypeWarningMask         = 1 << 6
-    , TWPBrainSignalTypeEventMask           = 1 << 7
-    , TWPBrainSignalTypeStatusWithheldMask  = 1 << 8
-    , TWPBrainSignalTypeCountryWithheldMask = 1 << 9
-    , TWPBrainSignalTypeUserWithheldMask    = 1 << 10
-    , TWPBrainSignalTypeControlMask         = 1 << 11
-    , TWPBrainSignalTypeDirectMessagesMask  = 1 << 12
+    { TWPBrainSignalTypeTweetMask           = 1U
+    , TWPBrainSignalTypeFriendsListsMask    = 1U << 1
+    , TWPBrainSignalTypeDeleteMask          = 1U << 2
+    , TWPBrainSignalTypeScrubGeoMask        = 1U << 3
+    , TWPBrainSignalTypeLimitMask           = 1U << 4
+    , TWPBrainSignalTypeDisconnectMask      = 1U << 5
+    , TWPBrainSignalTypeWarningMask         = 1U << 6
+    , TWPBrainSignalTypeEventMask           = 1U << 7
+    , TWPBrainSignalTypeStatusWithheldMask  = 1U << 8
+    , TWPBrainSignalTypeCountryWithheldMask = 1U << 9
+    , TWPBrainSignalTypeUserWithheldMask    = 1U << 10
+    , TWPBrainSignalTypeControlMask         = 1U << 11
+    , TWPBrainSignalTypeDirectMessagesMask  = 1U << 12
     };
 
 // TWPBrain class
 @interface TWPBrain : NSObject <OTCSTTwitterStreamingAPIDelegate>
 
+#pragma mark Initializations
++ ( instancetype ) wiseBrain;
+
+#pragma mark Registration of Limbs
+// Authenticating User
+- ( void ) registerLimbForAuthenticatingUser: ( NSObject <TWPLimb>* )_NewLimb
+                                 brainSignal: ( TWPBrainSignalTypeMask )_BrainSignals;
+
+- ( void ) removeLimbForAuthenticatingUser: ( NSObject <TWPLimb>* )_Limb
+                               brainSignal: ( TWPBrainSignalTypeMask )_BrainSignals;
+
+// Specifying User
+- ( void ) registerLimb: ( NSObject <TWPLimb>* )_Limb
+              forUserID: ( NSString* )_UserID
+            brainSignal: ( TWPBrainSignalTypeMask )_BrainSignals;
+
+- ( void ) removeLimb: ( NSObject <TWPLimb>* )_Limb
+            forUserID: ( NSString* )_UserID
+          brainSignal: ( TWPBrainSignalTypeMask )_BrainSignals;
+
 @end // TWPBrain class
 
 // TWPLimb class
 @protocol TWPLimb
+
+//- ( void )
 
 @end // TWPLimb class
 
--- Twipoker/TWPBrain.m
+++ Twipoker/TWPBrain.m
@@ -23,9 +23,131 @@
   ██████████████████████████████████████████████████████████████████████████████*/
 
 #import "TWPBrain.h"
+#import "TWPLoginUsersManager.h"
+
+// _TWPSignalLimbPair class
+@interface _TWPSignalLimbPair : NSObject
+
+@property ( assign, readwrite ) TWPBrainSignalTypeMask signalMask;
+@property ( strong, readwrite ) NSObject <TWPLimb>* limb;
+
+#pragma mark Initializations
++ ( instancetype ) pairWithSignalMask: ( TWPBrainSignalTypeMask )_SignalMask limb: ( NSObject <TWPLimb>* )_Limb;
+- ( instancetype ) initWithSignalMask: ( TWPBrainSignalTypeMask )_SignalMask limb: ( NSObject <TWPLimb>* )_Limb;
+
+#pragma mark Comparing
+- ( BOOL ) isEqualToPair: ( _TWPSignalLimbPair* )_RhsPair;
+
+@end
+
+@implementation _TWPSignalLimbPair
+
+@synthesize signalMask;
+@synthesize limb;
+
+#pragma mark Initializations
++ ( instancetype ) pairWithSignalMask: ( TWPBrainSignalTypeMask )_SignalMask limb: ( NSObject <TWPLimb>* )_Limb
+    {
+    return [ [ [ self class ] alloc ] initWithSignalMask: _SignalMask limb: _Limb ];
+    }
+
+- ( instancetype ) initWithSignalMask: ( TWPBrainSignalTypeMask )_SignalMask limb: ( NSObject <TWPLimb>* )_Limb
+    {
+    if ( !_Limb )
+        return nil;
+
+    if ( self = [ super init ] )
+        {
+        self.signalMask = _SignalMask;
+        self.limb = _Limb;
+        }
+
+    return self;
+    }
+
+#pragma mark Comparing
+- ( BOOL ) isEqualToPair: ( _TWPSignalLimbPair* )_RhsPair
+    {
+    if ( self == _RhsPair )
+        return YES;
+
+    return ( self.signalMask == _RhsPair.signalMask ) && ( self.limb == _RhsPair.limb );
+    }
+
+- ( BOOL ) isEqual: ( id )_Object
+    {
+    if ( self == _Object )
+        return YES;
+
+    if ( [ _Object isKindOfClass: [ _TWPSignalLimbPair class ] ] )
+        return [ self isEqualToPair: ( _TWPSignalLimbPair* )_Object ];
+
+    return [ super isEqual: _Object ];
+    }
+
+@end // _TWPSignalLimbPair class
 
 // TWPBrain class
 @implementation TWPBrain
+    {
+    // Home Timeline
+    STTwitterAPI __strong* _homeTimelineStreamAPI;
+    NSMutableArray __strong* _limbsSignalMaskPairsForAuthingUserStreamAPI;
+
+    // Specified Users
+    NSMutableDictionary __strong* _pairsForSecifiedUsersStreamAPI;
+    }
+
+#pragma mark Initializations
++ ( instancetype ) wiseBrain
+    {
+    return [ [ [ self class ] alloc ] init ];
+    }
+
+TWPBrain static __strong* sWiseBrain;
+- ( instancetype ) init
+    {
+    if ( !sWiseBrain )
+        {
+        if ( self = [ super init ] )
+            {
+            self->_homeTimelineStreamAPI = [ [ TWPLoginUsersManager sharedManager ] currentLoginUser ].twitterAPI;
+            self->_limbsSignalMaskPairsForAuthingUserStreamAPI = [ NSMutableArray array ];
+
+            [ self->_homeTimelineStreamAPI fetchUserStreamIncludeMessagesFromFollowedAccounts: @NO
+                                                                                     includeReplies: @NO
+                                                                                    keywordsToTrack: nil
+                                                                              locationBoundingBoxes: nil ];
+            sWiseBrain = self;
+            }
+        }
+
+    return sWiseBrain;
+    }
+
+#pragma mark Registration of Limbs
+// Authenticating User
+- ( void ) registerLimbForAuthenticatingUser: ( NSObject <TWPLimb>* )_NewLimb
+                                 brainSignal: ( TWPBrainSignalTypeMask )_BrainSignals
+    {
+    if ( _NewLimb )
+        {
+        _TWPSignalLimbPair* pair = [ _TWPSignalLimbPair pairWithSignalMask: _BrainSignals limb: _NewLimb ];
+
+        if ( pair )
+            [ self->_limbsSignalMaskPairsForAuthingUserStreamAPI addObject: pair ];
+        }
+    }
+
+- ( void ) removeLimbForAuthenticatingUser: ( NSObject <TWPLimb>* )_Limb
+                               brainSignal: ( TWPBrainSignalTypeMask )_BrainSignals
+    {
+    if ( _Limb )
+        {
+        [ self->_limbsSignalMaskPairsForAuthingUserStreamAPI removeObject:
+            [ _TWPSignalLimbPair pairWithSignalMask: _BrainSignals limb: _Limb ] ];
+        }
+    }
 
 @end // TWPBrain class
 
