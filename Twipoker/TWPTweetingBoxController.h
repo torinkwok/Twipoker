e2e78ac212e8d7afcbd6fe92d0dbcb8cb77f904e
Tong Guo <Tong-G@outlook.com>
2015-06-03 17:52:06 +0800

    Introduced `TWPTweetingBoxController` class

---
+++ Twipoker/TWPTweetingBoxController.h
@@ -0,0 +1,15 @@
+//
+//  TWPTweetingBoxController.h
+//  Twipoker
+//
+//  Created by Tong G. on 6/3/15.
+//  Copyright (c) 2015 Tong Guo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TWPTweetingBoxController : NSViewController
+
+
+
+@end
---
+++ Twipoker/TWPTweetingBoxController.m
@@ -0,0 +1,22 @@
+//
+//  TWPTweetingBoxController.m
+//  Twipoker
+//
+//  Created by Tong G. on 6/3/15.
+//  Copyright (c) 2015 Tong Guo. All rights reserved.
+//
+
+#import "TWPTweetingBoxController.h"
+
+@interface TWPTweetingBoxController ()
+
+@end
+
+@implementation TWPTweetingBoxController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
