ef0e30aceae9e94a0ef85826d22f1de87fec3a88
Tong Guo <Tong-G@outlook.com>
2015-06-11 07:34:00 +0800

    Replaced all "#import <Foundation/Foundation.h>" with "@import Foundation;"

--- Twipoker/TWPTweetUpdateObject.h
+++ Twipoker/TWPTweetUpdateObject.h
@@ -22,7 +22,7 @@
   ████████████████████████████████████████████████████████████████████████████████
   ██████████████████████████████████████████████████████████████████████████████*/
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 @interface TWPTweetUpdateObject : NSObject
 
