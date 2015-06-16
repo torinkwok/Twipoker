2f76c87b1780eeaee94d90f73127951bc04e29f7
Tong Guo <Tong-G@outlook.com>
2015-06-16 16:26:41 +0800

    Introduced `TWPTwitterUserProfileView` class

---
+++ Twipoker/TWPTwitterUserProfileView.h
@@ -0,0 +1,78 @@
+/*=============================================================================┐
+|             _  _  _       _                                                  |  
+|            (_)(_)(_)     | |                            _                    |██
+|             _  _  _ _____| | ____ ___  ____  _____    _| |_ ___              |██
+|            | || || | ___ | |/ ___) _ \|    \| ___ |  (_   _) _ \             |██
+|            | || || | ____| ( (__| |_| | | | | ____|    | || |_| |            |██
+|             \_____/|_____)\_)____)___/|_|_|_|_____)     \__)___/             |██
+|                                                                              |██
+|                 _______    _             _                 _                 |██
+|                (_______)  (_)           | |               | |                |██
+|                    _ _ _ _ _ ____   ___ | |  _ _____  ____| |                |██
+|                   | | | | | |  _ \ / _ \| |_/ ) ___ |/ ___)_|                |██
+|                   | | | | | | |_| | |_| |  _ (| ____| |    _                 |██
+|                   |_|\___/|_|  __/ \___/|_| \_)_____)_|   |_|                |██
+|                             |_|                                              |██
+|                                                                              |██
+|                         Copyright (c) 2015 Tong Guo                          |██
+|                                                                              |██
+|                             ALL RIGHTS RESERVED.                             |██
+|                                                                              |██
+└==============================================================================┘██
+  ████████████████████████████████████████████████████████████████████████████████
+  ██████████████████████████████████████████████████████████████████████████████*/
+
+#import <Cocoa/Cocoa.h>
+
+@class TWPUserAvatarWell;
+@class TWPCuttingLineView;
+@class TWPNavigationBarController;
+
+@interface TWPTwitterUserProfileView : NSView
+    {
+@private
+    OTCTwitterUser __strong* _twitterUser;
+    TWPNavigationBarController __weak* _navigationBarController;
+    }
+
+@property ( weak ) IBOutlet NSButton* hideButton;
+@property ( weak ) IBOutlet TWPCuttingLineView* cuttingLineView;
+
+@property ( weak ) IBOutlet TWPUserAvatarWell* userAvatar;
+@property ( weak ) IBOutlet NSTextField* userDisplayNameField;
+@property ( weak ) IBOutlet NSTextField* userScreenNameField;
+
+@property ( weak ) IBOutlet NSButton* tweetsCountButton;
+@property ( weak ) IBOutlet NSButton* followersCountButton;
+@property ( weak ) IBOutlet NSButton* followingCountButton;
+
+@property ( weak ) IBOutlet NSButton* tweetToUserButton;
+@property ( weak ) IBOutlet NSButton* sendADirectMessageButton;
+@property ( weak ) IBOutlet NSButton* addOrRemoveFromListsButton;
+
+@property ( weak ) IBOutlet NSButton* iDoNotLikeThisGuyButton;
+
+@property ( strong, readwrite ) OTCTwitterUser* twitterUser;
+
+@end
+
+/*=============================================================================┐
+|                                                                              |
+|                                        `-://++/:-`    ..                     |
+|                    //.                :+++++++++++///+-                      |
+|                    .++/-`            /++++++++++++++/:::`                    |
+|                    `+++++/-`        -++++++++++++++++:.                      |
+|                     -+++++++//:-.`` -+++++++++++++++/                        |
+|                      ``./+++++++++++++++++++++++++++/                        |
+|                   `++/++++++++++++++++++++++++++++++-                        |
+|                    -++++++++++++++++++++++++++++++++`                        |
+|                     `:+++++++++++++++++++++++++++++-                         |
+|                      `.:/+++++++++++++++++++++++++-                          |
+|                         :++++++++++++++++++++++++-                           |
+|                           `.:++++++++++++++++++/.                            |
+|                              ..-:++++++++++++/-                              |
+|                             `../+++++++++++/.                                |
+|                       `.:/+++++++++++++/:-`                                  |
+|                          `--://+//::-.`                                      |
+|                                                                              |
+└=============================================================================*/
---
+++ Twipoker/TWPTwitterUserProfileView.m
@@ -0,0 +1,89 @@
+/*=============================================================================┐
+|             _  _  _       _                                                  |  
+|            (_)(_)(_)     | |                            _                    |██
+|             _  _  _ _____| | ____ ___  ____  _____    _| |_ ___              |██
+|            | || || | ___ | |/ ___) _ \|    \| ___ |  (_   _) _ \             |██
+|            | || || | ____| ( (__| |_| | | | | ____|    | || |_| |            |██
+|             \_____/|_____)\_)____)___/|_|_|_|_____)     \__)___/             |██
+|                                                                              |██
+|                 _______    _             _                 _                 |██
+|                (_______)  (_)           | |               | |                |██
+|                    _ _ _ _ _ ____   ___ | |  _ _____  ____| |                |██
+|                   | | | | | |  _ \ / _ \| |_/ ) ___ |/ ___)_|                |██
+|                   | | | | | | |_| | |_| |  _ (| ____| |    _                 |██
+|                   |_|\___/|_|  __/ \___/|_| \_)_____)_|   |_|                |██
+|                             |_|                                              |██
+|                                                                              |██
+|                         Copyright (c) 2015 Tong Guo                          |██
+|                                                                              |██
+|                             ALL RIGHTS RESERVED.                             |██
+|                                                                              |██
+└==============================================================================┘██
+  ████████████████████████████████████████████████████████████████████████████████
+  ██████████████████████████████████████████████████████████████████████████████*/
+
+#import "TWPTwitterUserProfileView.h"
+
+@implementation TWPTwitterUserProfileView
+
+@synthesize hideButton;
+@synthesize cuttingLineView;
+
+@synthesize userAvatar;
+@synthesize userDisplayNameField;
+@synthesize userScreenNameField;
+
+@synthesize tweetsCountButton;
+@synthesize followersCountButton;
+@synthesize followingCountButton;
+
+@synthesize tweetToUserButton;
+@synthesize sendADirectMessageButton;
+@synthesize addOrRemoveFromListsButton;
+
+@synthesize iDoNotLikeThisGuyButton;
+
+@dynamic twitterUser;
+
+#pragma mark Dynamic Accessors
+- ( void ) setTwitterUser: ( OTCTwitterUser* )_TwitterUser
+    {
+    self->_twitterUser = _TwitterUser;
+
+    // TODO:
+    }
+
+- ( OTCTwitterUser* ) twitterUser
+    {
+    return self->_twitterUser;
+    }
+
+#pragma mark Custom Drawing
+- ( void ) drawRect: ( NSRect )_DirtyRect
+    {
+    [ [ NSColor whiteColor ] set ];
+    NSRectFill( _DirtyRect );
+    }
+
+@end
+
+/*=============================================================================┐
+|                                                                              |
+|                                        `-://++/:-`    ..                     |
+|                    //.                :+++++++++++///+-                      |
+|                    .++/-`            /++++++++++++++/:::`                    |
+|                    `+++++/-`        -++++++++++++++++:.                      |
+|                     -+++++++//:-.`` -+++++++++++++++/                        |
+|                      ``./+++++++++++++++++++++++++++/                        |
+|                   `++/++++++++++++++++++++++++++++++-                        |
+|                    -++++++++++++++++++++++++++++++++`                        |
+|                     `:+++++++++++++++++++++++++++++-                         |
+|                      `.:/+++++++++++++++++++++++++-                          |
+|                         :++++++++++++++++++++++++-                           |
+|                           `.:++++++++++++++++++/.                            |
+|                              ..-:++++++++++++/-                              |
+|                             `../+++++++++++/.                                |
+|                       `.:/+++++++++++++/:-`                                  |
+|                          `--://+//::-.`                                      |
+|                                                                              |
+└=============================================================================*/
