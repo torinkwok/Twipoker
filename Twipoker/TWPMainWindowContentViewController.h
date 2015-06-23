01678ee5c3e626bdacb9efa3a78dc661f2aa7503
Tong Guo <Tong-G@outlook.com>
2015-06-23 15:11:41 +0800

    Introduced view controller for main window content view

---
+++ Twipoker/TWPMainWindowContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  TWPMainWindowContentViewController.h
+//  Twipoker
+//
+//  Created by Tong G. on 6/23/15.
+//  Copyright (c) 2015 Tong Guo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TWPMainWindowContentViewController : NSViewController
+
+@end
---
+++ Twipoker/TWPMainWindowContentViewController.m
@@ -0,0 +1,22 @@
+//
+//  TWPMainWindowContentViewController.m
+//  Twipoker
+//
+//  Created by Tong G. on 6/23/15.
+//  Copyright (c) 2015 Tong Guo. All rights reserved.
+//
+
+#import "TWPMainWindowContentViewController.h"
+
+@interface TWPMainWindowContentViewController ()
+
+@end
+
+@implementation TWPMainWindowContentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
