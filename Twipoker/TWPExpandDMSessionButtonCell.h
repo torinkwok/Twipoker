395b71c7873aca9764279c906bd3524edefef574
Tong Guo <Tong-G@outlook.com>
2015-07-13 21:32:55 +0800

    Backup

---
+++ Twipoker/TWPExpandDMSessionButtonCell.h
@@ -0,0 +1,13 @@
+//
+//  TWPExpandDMSessionButtonCell.h
+//  Twipoker
+//
+//  Created by Tong G. on 7/13/15.
+//  Copyright © 2015 Tong Guo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TWPExpandDMSessionButtonCell : NSButtonCell
+
+@end
---
+++ Twipoker/TWPExpandDMSessionButtonCell.m
@@ -0,0 +1,20 @@
+//
+//  TWPExpandDMSessionButtonCell.m
+//  Twipoker
+//
+//  Created by Tong G. on 7/13/15.
+//  Copyright © 2015 Tong Guo. All rights reserved.
+//
+
+#import "TWPExpandDMSessionButtonCell.h"
+
+@implementation TWPExpandDMSessionButtonCell
+
+- ( void ) drawWithFrame: ( NSRect )_Frame
+                  inView: ( nonnull NSView* )_ControlView
+    {
+    [ [ NSColor blueColor ] set ];
+    NSRectFill( _Frame );
+    }
+
+@end
